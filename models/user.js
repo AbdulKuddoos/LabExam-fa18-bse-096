15e534bbf0b403e63c179e689181f66dea571ed2
Syed Ali Haider Abedi <53150433+AliHaider@users.noreply.github.com>
2021-06-22 11:08:43 +0500

    updation

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var UserSchema = mongoose.Schema({
   name: String,
   email: String,
+  gender: String,
   password: String,
 });
 const User = mongoose.model("User", UserSchema);
